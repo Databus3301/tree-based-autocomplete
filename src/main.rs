c61491cd5f377d371d99c9dc436cd9f35de74cbf
databus3301 <felix@rose.ruhr>
2024-08-31 12:39:27 +0200

    clean up + dict fix

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod tree;
 mod input;
 
 fn main() {
-    let contents = std::fs::read_to_string("./res/de-100k.txt").unwrap();
+    let contents = std::fs::read_to_string("./res/en-80k.txt").unwrap();
     let words = contents.split("\n").map(|x| x.split_whitespace().nth(0).unwrap()).collect::<Vec<&str>>();
 
     // Build tree
     let time = SystemTime::now();
-    let mut t = Tree::<char>::build_from_dict(words);
+    let t = Tree::<char>::build_from_dict(words);
     println!("Took {}ms to construct the tree", SystemTime::duration_since(&SystemTime::now(), time).unwrap().as_millis());
 
     // Query suggestions
